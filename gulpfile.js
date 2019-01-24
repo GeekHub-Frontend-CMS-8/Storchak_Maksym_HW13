6278dfe5f15e1d852b8a8b6ebe4f10dd4905555b
maksymstorchak <maksym89storchak@gmail.com>
2019-01-24 17:34:10 +0200

    todos

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+gulp.task('sass', function () {
+	return gulp.src('assets/sass/**/*.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('build/css'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('img', function () {
+	return gulp.src('assets/img/**/*')
+		.pipe(gulp.dest('build/img'))
+		.pipe(browserSync.reload({stream: true}))
+
+});
+
+gulp.task("html", function () {
+	return gulp.src("assets/**/*.html")
+		.pipe(gulp.dest("build"))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function () {
+	return gulp.src('assets/script/**/*.js')
+		.pipe(gulp.dest('build/script'))
+		.pipe(browserSync.reload({stream: true}))
+});
+gulp.task("watch", [ 'sass', "html", 'img', 'js'], function () {
+	browserSync.init({
+		server: "./build",
+		notify: false,
+		ui: {
+			port: 3000
+		}
+    });
+    gulp.watch('assets/sass/**/*.sass', ["sass"]);
+	gulp.watch('assets/**/*.html' , ['html']);
+	gulp.watch('assets/script/**/*.js' , ['js']);
+	gulp.watch('assets/img/**/*', ["img"]);
+});
