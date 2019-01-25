fe284fc1f030b1887ee91436f63dd2a5089d3f2a
maksymstorchak <maksym89storchak@gmail.com>
2019-01-25 15:35:35 +0200

    ajax

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,23 +1,86 @@
-function buttonClick ()
-{
-  var div = document.getElementById ("myDiv");
-  var button = document.getElementById ("myButton");
-  var content_div = document.getElementById ("ListContent");
-  var textarea = document.getElementById ("myTextarea");
-
-  if (div.contentEditable == "true")
-  {
-    div.contentEditable = "false";
-    content_div.style.display = "inline";
-    textarea.innerHTML = div.innerHTML;
-    button.value = "Редактировать";
+jQuery(function ($) {
+  let todos = [];
+  const list = $('#todolist');
+  const input = $('input');
+  function addTodo(value, state) {
+      todos.push({
+          value,
+          state: state || false
+      });
+      renderTodos();
   }
-
-  else
-  {
-    div.contentEditable = "true";
-    content_div.style.display = "none";
-    button.value = "Сохранить";
+  function removeTodo(index) {
+      todos.splice(index, 1);
+      renderTodos();
   }
+  function setTodo(index, value) {
+      todos[index].value = value;
+      renderTodos();
+  }
+  function checkTodo(index) {
+      todos[index].state = !todos[index].state;
+      renderTodos();
+  }
+  function saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(todos));
+  }
+  function loadTodos() {
+      todos = JSON.parse(localStorage.getItem('todos'));
+      renderTodos();
+  }
+  function renderTodos() {
+      list.empty();
+      $.each(todos, function (i) {
+          list.append(
+              `
+          <li>
+          ${this.state ? '<i data-type="state" class="far fa-check-circle"></i>' : '<i data-type="state" class="far fa-circle"></i>'}
+          ${this.value}
+          <i data-type="edit" class="fas fa-pencil-alt"></i>
+          <i data-type="delete" class="fas fa-trash-alt"></i>    
+          </li>
+          `
+          );
 
-}
+      });
+  }
+  input.on('change', function (e) {
+      console.log(input.data('idx'));
+      if (input.data('idx') === undefined) {
+          addTodo(this.value);
+      } else {
+          setTodo(input.data('idx'), this.value);
+          input.data('idx', undefined);
+      }
+      this.value = '';
+  });
+  $('#todolist').on('click', function (e) {
+      const target = $(e.target);
+      const li = $(e.target.parentElement);
+      const idx = li.index();
+      if (target.data('type') == 'delete') {
+          removeTodo(idx);
+      } else if (target.data('type') == 'edit') {
+          input.val(todos[idx].value);
+          input.data('idx', idx);
+
+      } else if (target.data('type') == 'state') {
+          checkTodo(idx);
+      }
+  });
+  $('#save').on('click', function (e) {
+      saveTodos();
+  });
+  $('#loadmore').on('click', function (e) {
+      $.get(
+          "https://jsonplaceholder.typicode.com/todos",{}, onAjaxSuccess, 'json'
+      );
+      function onAjaxSuccess(data, textStatus) {
+          const arr = data.slice(0, 20);
+          for (let i = 0; i<arr.length; i++) {
+              addTodo(arr[i].title, arr[i].completed);
+          }
+      }
+  });
+  loadTodos();
+});
